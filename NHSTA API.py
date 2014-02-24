3a854ac821b5f677c73ea123c44cf98578de33d8
jaydior1 <sweedhi2@hotmail.com>
2014-02-24 16:34:40 -0500

    Added ChangeLog for all subsequent changes

--- NHSTA API.py
+++ NHSTA API.py
@@ -1,15 +1,28 @@
 from urllib2 import urlopen
 from json import load
 
-apiUrl = "http://www.nhtsa.gov/webapi/api/SafetyRatings"
-apiParam = "/vehicleid/7654"
-outputFormat = "?format=json"
-#Combine all three variables to make up the complete request URL
-response = urlopen(apiUrl + apiParam + outputFormat)
 
+def Choose_year(year):
+    if year != "":
+        apiUrl = "http://www.nhtsa.gov/webapi/api/SafetyRatings"
+        outputFormat = "?format=json"
+        apiParam = "/modelyear/" + year
+        #Combine all three variables to make up the complete request URL
+        response = urlopen(apiUrl + apiParam + outputFormat)
+        return response
+    else:
+        apiUrl = "http://www.nhtsa.gov/webapi/api/SafetyRatings"
+        outputFormat = "?format=json"
+        apiParam = ""
+        response = urlopen(apiUrl + apiParam + outputFormat)
+        return response
+    
+year = str(raw_input("Choose a year(1990-2015):"))
+    
 #code below is only to handle JSON response object/format
 #use equivalent sets of commands to handle xml response object/format
-json_obj = load(response)
+json_obj = load(Choose_year(year))
+
 
 #Load the Result (vehicle collection) from the JSON response
 print '------------------------------'
